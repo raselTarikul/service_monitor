44d7c5636aa32379b9515c30a114df2741df34e0
Tarikul Islam <ti.rasel@gmail.com>
2020-07-13 00:07:05 +0800

    medn

--- src/monitor/settings/test.py
+++ src/monitor/settings/test.py
@@ -7,7 +7,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST': 'test_db',
+        'TEST': { 
+            'NAME': 'mytestdatabase',
+            },
     }
 }
 
